e15ab10f1b2c6a294860a63ff10a72d3b38c6fbe
Hukkatir <dudinassonya@gmail.com>
2024-11-07 00:18:58 +0300

    переделать localStorage

---
+++ BlazorClient/BlazorUser.cs
@@ -0,0 +1,19 @@
+//пусть будет, лишним не будет
+public class User
+{
+    public int UserId { get; set; }
+    public string Username { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string Pass { get; set; } = null!;
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public DateTime RegistrationDate { get; set; }
+    public DateTime? LastLoginDate { get; set; }
+    public int RoleId { get; set; }
+}
+
+
+public class BlazorUser
+{
+    public User User { get; set; }
+}
