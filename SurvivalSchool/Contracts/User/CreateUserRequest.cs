aa5eda3493e107c603bd4a22cecc9b4439b11228
Hukkatir <dudinassonya@gmail.com>
2024-11-06 13:40:54 +0300

    Gitignore (10q MeidKoi)

---
+++ SurvivalSchool/Contracts/User/CreateUserRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace SurvivalSchool.Contracts.User
+{
+    public class CreateUserRequest
+    {
+        public string Username { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Pass { get; set; } = null!;
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public int RoleId { get; set; }
+    }
+}
